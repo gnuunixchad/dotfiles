af86c4359c4f3a553fe6d55e5d9b924dfb9ffb5c
nate zhou <gnuunixchad@outlook.com>
2025-10-20 19:01:22 +0800

    Add(functions.bash): 'hist()' for history switch

--- .config/bash/functions.bash
+++ .config/bash/functions.bash
@@ -24,3 +24,50 @@ help() { # read help with a pager
     [ "$?" -eq 0 ] && echo "$help_output" | /usr/bin/less -i
 }
 
+# shell history switch
+hist() {
+    PROFILE="${HOME}/.profile"
+
+    status() {
+        if [ -z "$HISTFILE" ]; then
+            echo "[bash]: history is disabled"
+        else
+            echo "[bash]: history is enabled"
+        fi
+    }
+
+    disable() {
+        unset HISTFILE
+        status
+    }
+
+    enable() {
+        eval "$( grep '^export HISTFILE=' $PROFILE)"
+        status
+    }
+
+    help() {
+        cat <<_EOF_
+USAGE
+        $(basename "$0") [OPTIONS]
+OPTIONS
+        -d,--disable    disable shell history for current session
+        -e,--enable     enable shell history for current session
+        -f,--file       print shell history file for current session
+        -h,--help       print this help info
+_EOF_
+    }
+
+    [ -z "$1" ] && status
+
+    while [ -n "$1" ]; do
+        case "$1" in
+            -d|--disable) disable;;
+            -e|--enable) enable;;
+            -f|--file) echo "[bash]: history is saved to $HISTFILE";;
+            -h|--help) help;;
+            *) help;;
+        esac
+        shift
+    done
+}
