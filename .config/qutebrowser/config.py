e65bd7ede5b5fe6c21e1b4d4fa2a8b1ab1c7500c
nate zhou <gnuunixchad@outlook.com>
2025-12-08 14:00:15 +0800

    Feat(qutebrowser/config.py): Editor transparency

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -11,7 +11,7 @@ config.load_autoconfig(False) # ignore GUI settings
 
 # Config ######################################################################
 # insert mode <^e>
-c.editor.command = ['foot', '-T', 'Floating_Term', 'nvim', '{file}', '-c', 'normal {line}G{column0}l']
+c.editor.command = ['foot', '-T', 'Floating_Term', '-o', 'colors.alpha=0.9', 'nvim', '{file}', '-c', 'normal {line}G{column0}l']
 
 c.url.start_pages = 'file:///dev/null'
 c.url.default_page= 'file:///dev/null'
