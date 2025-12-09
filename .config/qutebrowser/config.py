7d593da85a1ddc403329f7ce47f0d2f2d401d526
nate zhou <gnuunixchad@outlook.com>
2025-12-09 12:56:51 +0800

    Mod(qutebrowser): Hide tab bar with only one tab

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -22,7 +22,7 @@ c.fonts.default_size = '14pt' # UI
 
 c.tabs.last_close = "startpage"
 
-c.tabs.show = "always"
+c.tabs.show = "multiple"
 c.tabs.position = "left"
 c.tabs.padding = {"bottom":0, "left":0, "right":0, "top":0}
 c.tabs.min_width = 0
