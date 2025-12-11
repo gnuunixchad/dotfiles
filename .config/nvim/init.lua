59c2eddba7f4d2a51740a3a2e201ad125626414d
nate zhou <gnuunixchad@outlook.com>
2025-12-11 16:31:21 +0800

    Refact(neovim): Separate plugin configs
    Add `.config/nvim/lua/plugins/init.lua`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,23 +7,4 @@ require('config.colors')
 require('config.options')
 require('config.autocmd')
 require('config.bindings')
-
-
-vim.cmd [[
-call plug#begin()
-Plug 'norcalli/nvim-colorizer.lua'
-Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'neoclide/coc.nvim', { 'branch': 'release' }
-Plug 'lukas-reineke/indent-blankline.nvim'
-Plug 'MeanderingProgrammer/render-markdown.nvim'
-Plug 'tpope/vim-fugitive'
-Plug 'voldikss/vim-floaterm'
-Plug 'ptzz/lf.vim'
-call plug#end()
-]]
-
-require('plugins.nvim-colorizer')
-require('plugins.nvim-treesitter')
-require('plugins.indent-blankline')
-require('plugins.render-markdown')
-require('plugins.vim-floaterm')
+require('plugins.init')
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,23 @@
+-- vim:ft=lua
+-- nvim/lua/plugins/init.lua
+-- @author nate zhou
+-- @since 2025
+
+vim.cmd [[
+call plug#begin()
+Plug 'norcalli/nvim-colorizer.lua'
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'neoclide/coc.nvim', { 'branch': 'release' }
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'MeanderingProgrammer/render-markdown.nvim'
+Plug 'tpope/vim-fugitive'
+Plug 'voldikss/vim-floaterm'
+Plug 'ptzz/lf.vim'
+call plug#end()
+]]
+
+require('plugins.nvim-colorizer')
+require('plugins.nvim-treesitter')
+require('plugins.indent-blankline')
+require('plugins.render-markdown')
+require('plugins.vim-floaterm')
