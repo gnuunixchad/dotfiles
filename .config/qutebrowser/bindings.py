2d626d2e38b6c3e5429859ad849b5f3a2ce2ecc1
nate zhou <gnuunixchad@outlook.com>
2025-12-22 19:09:20 +0800

    Feat(qutebrowser/bindings.py): canvas reading warn
    - Mod: Change keybindings and commands to {en,dis}able canvas reading.
    - Feat: Display an messages in the statusbar after action.

--- .config/qutebrowser/bindings.py
+++ .config/qutebrowser/bindings.py
@@ -33,8 +33,9 @@ config.bind('zi', 'zoom-in')
 config.bind('zo', 'zoom-out')
 config.bind('zz', 'zoom {}'.format(c.zoom.default))
 
-# Set to False in `config.py`, manually toggle it if website's uploading breaks
-config.bind('zcs', 'config-cycle content.canvas_reading True False')
+# manually toggle canvas reading if some website's breaking
+config.bind('zcR', 'set content.canvas_reading true ;; message-warning "Canvas Reading: True"')
+config.bind('zcr', 'set content.canvas_reading false ;; message-info "Canvas Reading: False"')
 
 config.bind('.', 'config-cycle tabs.show always never')
 config.bind(',', 'config-cycle tabs.width 20% 10%')
