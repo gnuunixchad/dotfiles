cd3ff2f34847ab88adbbfc6cbd97c31e79606f37
nate zhou <gnuunixchad@outlook.com>
2025-12-23 19:32:48 +0800

    Refact(nvim-treesitter.lua): Compatibility notes

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,7 @@
 -- nvim/lua/plugins/nvim-treesitter.lua
 -- @author nate zhou
 -- @since 2025
+-- This config only works on nvim-treesitter master branch since December 2025.
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = { "bash", "diff", "markdown", "markdown_inline", "c", "java", "python", "go", "gotmpl", "vim", "css", "json", "make", "ssh_config", "html"},
