2812e075f3678a1bbf0743c4a01ca9b1341de249
nate zhou <gnuunixchad@outlook.com>
2025-12-27 09:25:34 +0800

    Mod(nvim-treesitter.lua): Disable for `{,ba}sh`
    `tree-sitter-bash` has regressions compared to vim's native syntax
    highlighting, e.g. parentheses paring. This parser is only useful in
    markdown code blocks.

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,10 +5,19 @@
 -- This config only works on nvim-treesitter master branch since December 2025.
 
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "bash", "diff", "markdown", "markdown_inline", "c", "java", "python", "go", "gotmpl", "vim", "css", "json", "make", "ssh_config", "html"},
+    ensure_installed = { 'bash', 'diff', 'markdown', 'markdown_inline', 'c', 'java', 'python', 'go', 'gotmpl', 'vim', 'css', 'json', 'make', 'ssh_config', 'html'},
 
     highlight = {
         enable = true,
+        disable = { 'sh', 'bash' },
         additional_vim_regex_highlighting = false,
   },
 }
+
+-- another way is to set `highlight.enable` to false and whitelist filetypes
+--vim.api.nvim_create_autocmd('FileType', {
+--    pattern = { 'markdown', 'markdown_inline', 'java' }
+--    callback = function()
+--        vim.treesitter.start()
+--    end,
+--})
