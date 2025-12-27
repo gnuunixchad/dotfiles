d20dc4b606f660881e3756f2044ab69dd0d64808
nate zhou <gnuunixchad@outlook.com>
2025-12-27 14:57:49 +0800

    Feat(completion.bash): Support more scripts
    Add support for:
        - `.local/bin/seal`
        - `.local/bin/phone`
        - `.local/bin/address`
        - `.local/bin/capture`
        - `.local/bin/bright`
    
    TODO: completion for `.local/bin/audio`

--- .config/bash/completion.bash
+++ .config/bash/completion.bash
@@ -7,8 +7,47 @@ _saddle() {
     local options
     local current_word="${COMP_WORDS[COMP_CWORD]}"
     options="-m --mount -u --unmount -p --power-off -e --eject"
-
     COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
-
 }
 complete -F _saddle saddle
+
+_seal() {
+    local options
+    local current_word="${COMP_WORDS[COMP_CWORD]}"
+    options="-c --cipher -e --encrypt -d --decrypt -r -rifle -s --sign
+             -v --verify"
+    COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
+}
+complete -F _seal seal
+
+_phone() {
+    local options
+    local current_word="${COMP_WORDS[COMP_CWORD]}"
+    options="-l --list -m --mount -u --unmount"
+    COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
+}
+complete -F _phone phone
+
+_address() {
+    local options
+    local current_word="${COMP_WORDS[COMP_CWORD]}"
+    options="-e --edit"
+    COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
+}
+complete -F _address address
+
+_capture() {
+    local options
+    local current_word="${COMP_WORDS[COMP_CWORD]}"
+    options="-6 --60 -m --mkv -M --mute"
+    COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
+}
+complete -F _capture capture
+
+_bright() {
+    local options
+    local current_word="${COMP_WORDS[COMP_CWORD]}"
+    options="--minus --minus10 --plus --plus10 --max --min --no-fifo"
+    COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
+}
+complete -F _bright bright
