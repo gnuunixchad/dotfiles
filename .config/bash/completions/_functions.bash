9988bda4ad10b9ef358dc01e9cf596745191b143
nate zhou <gnuunixchad@outlook.com>
2026-01-01 12:04:50 +0800

    Feat(_functions.bash): packages completion
    Add packages completion for functions in `.config/shell/functions.sh`:
        - `plb()`
        - `pll()`
        - `pld()`
        - `what()`
        - `diffpkg()`

--- .config/bash/completions/_functions.bash
+++ .config/bash/completions/_functions.bash
@@ -10,3 +10,15 @@ _hist() {
     COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
 }
 complete -F _hist hist
+
+_package() {
+    local options
+    local current_word="${COMP_WORDS[COMP_CWORD]}"
+    options="$(pacman -Qq)"
+    COMPREPLY=($(compgen -W "${options}" -- ${current_word}))
+}
+complete -F _package plb
+complete -F _package pll
+complete -F _package pld
+complete -F _package what
+complete -F _package diffpkg
