7efacb850c8677c689ff032dc204613b9de7966a
nate zhou <gnuunixchad@outlook.com>
2026-03-20 20:24:19 +0800

    Mod(swayimg): set info text for modes

--- .config/swayimg/modes/viewer.lua
+++ .config/swayimg/modes/viewer.lua
@@ -1,3 +1,23 @@
 -- swayimg/modes/viewer.lua
 -- @author nate zhou
 -- @since 2026
+
+swayimg.viewer.set_text("topleft", {
+  "{name}",
+  "{format}",
+  "{sizehr}",
+  "{time}",
+  "{meta.Exif.Photo.DateTimeOriginal}",
+  "{meta.Exif.Image.Model}"
+})
+swayimg.viewer.set_text("topright", {
+    "{list.index}/{list.total}"
+})
+swayimg.viewer.set_text("bottomleft", {
+    "{frame.index}/{frame.total}",
+    "{frame.width}x{frame.height}",
+    "{scale}"
+})
+swayimg.viewer.set_text("bottomright", {
+    "{path}"
+})
