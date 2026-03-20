51571f14328565e3c5d76f439304cc0036d830f3
nate zhou <gnuunixchad@outlook.com>
2026-03-20 19:07:20 +0800

    Mod(swayimg): configure imagelist and font

--- .config/swayimg/init.lua
+++ .config/swayimg/init.lua
@@ -5,3 +5,13 @@
 require('modes.viewer')
 require('modes.gallery')
 require('modes.slideshow')
+
+swayimg.imagelist.set_order("none")
+
+swayimg.text.set_timeout(0)
+swayimg.text.hide()
+swayimg.text.set_font("SourceCodePro")
+swayimg.text.set_size(20)
+swayimg.text.set_padding(2)
+swayimg.text.set_foreground(0xffdddddd)
+swayimg.text.set_shadow(0xff222222)
