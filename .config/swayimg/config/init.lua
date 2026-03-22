085071124a57939465ad7822805dfd550bd2a8cf
nate zhou <gnuunixchad@outlook.com>
2026-03-22 19:34:32 +0800

    Mod(swayimg): Set scale to optimal on window resize
    https://github.com/artemsen/swayimg/issues/432

--- .config/swayimg/config/init.lua
+++ .config/swayimg/config/init.lua
@@ -4,3 +4,7 @@
 
 swayimg.set_mode("viewer")
 swayimg.imagelist.set_order("none")
+
+swayimg.on_window_resize(function()
+  swayimg.viewer.set_fix_scale("optimal")
+end)
--- .config/swayimg/modes/viewer/init.lua
+++ .config/swayimg/modes/viewer/init.lua
@@ -5,7 +5,7 @@
 require("modes/viewer/text")
 require("modes/viewer/bindings")
 
-swayimg.viewer.set_default_scale(1.0)
+swayimg.viewer.set_default_scale("optimal")
 
 swayimg.viewer.set_window_background(0xff111111)
 swayimg.viewer.set_image_chessboard(20, 0xff333333, 0xff4c4c4c)
