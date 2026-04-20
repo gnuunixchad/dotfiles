4bbf108b45e7b80351eef6fe38124f4c2d2ae669
nate zhou <gnuunixchad@outlook.com>
2026-04-20 22:09:35 +0800

    Mod(swayimg/modes/gallery): increase border size

--- .config/swayimg/modes/gallery/init.lua
+++ .config/swayimg/modes/gallery/init.lua
@@ -10,7 +10,7 @@ swayimg.gallery.set_selected_scale(1.2)
 swayimg.gallery.set_aspect("keep")
 
 swayimg.gallery.set_thumb_size(200)
-swayimg.gallery.set_border_size(1)
+swayimg.gallery.set_border_size(4)
 swayimg.gallery.set_border_color(0xffeeeeee)
 
 swayimg.gallery.set_window_color(0xff111111)
