a8ee535a08829cb05782843cccfa6ab17208cc0b
nate zhou <gnuunixchad@outlook.com>
2026-04-26 12:20:37 +0800

    Mod(swayimg): reload image after delete in functions.lua

--- .config/swayimg/modes/slideshow/functions.lua
+++ .config/swayimg/modes/slideshow/functions.lua
@@ -21,6 +21,7 @@ function M.delete()
         os.remove(image.path)
         os.execute("pkill -SIGUSR1 swayimg")
         os.execute(string.format("notify-send -t 1100 -u low -r 3301 'swayimg' 'Deleted %s'", image.path))
+        swayimg.slideshow.reload()
     end
 end
 
